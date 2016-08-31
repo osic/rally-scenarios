d193bb74ed7c5ff5976a3127ca27c56d6ba3af93
Steve Heyman <steve.heyman@rackspace.com>
2016-08-31 13:21:26 -0500

    Update run_rally.sh

--- run_rally.sh
+++ run_rally.sh
@@ -4,7 +4,7 @@ rally-manage db recreate
 echo "Create a Rally deployment"
 rally deployment create --file=existing.json --name=existing
 echo "Start the Rally task"
-rally -vd task start rally/samples/tasks/scenarios/nova/osic-2.yaml --task-args 'servers_to_build: 5' > rally-console.out 2>&1
+rally -vd task start {{RALLY_TASK}} {{RALLY_PARMS}} > rally-console.out 2>&1
 rally task report --junit --out results.xml
 rally task report --html-static --out results.html
 rally task results > results.json
