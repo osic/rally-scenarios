871854232c55bbe4eefedf85bcbd19f9893cc7f2
Steve Heyman <steve.heyman@rackspace.com>
2016-09-06 11:23:56 -0500

    Update run_rally.sh

--- run_rally.sh
+++ run_rally.sh
@@ -4,6 +4,12 @@ echo "Initialize Rally database"
 rally-manage db recreate
 echo "Create a Rally deployment"
 rally deployment create --file=existing.json --name=existing
+echo "Start the Rally prime task"
+rally -vd task start {{RALLY_PRIME_TASK}} {{RALLY_PRIME_PARMS}} > rally-prime-console.out 2>&1
+rally task report --junit --out prime-results.xml
+rally task report --html-static --out prime-results.html
+rally task results > prime-results.json
+
 echo "Start the Rally task"
 rally -vd task start {{RALLY_TASK}} {{RALLY_PARMS}} > rally-console.out 2>&1
 rally task report --junit --out results.xml
