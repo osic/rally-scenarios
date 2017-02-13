b1c88fd5b4527158beeb60b44015315e8c3ec587
jwatford <jonte.watford@intel.com>
2017-02-13 14:28:26 -0600

    Update extract_values.py

--- extract_values.py
+++ extract_values.py
@@ -1,5 +1,6 @@
 import yaml
 import json
+import configparser
 
 with open('/etc/ansible/roles/os_tempest/defaults/main.yml') as f:
   data = f.read()
@@ -7,16 +8,17 @@ with open('args.yaml') as f:
   args = f.read()
 with open('/etc/ansible/roles/os_cinder/defaults/main.yml') as f:
   cinder_backends = f.read()
-with open('/etc/ansible/roles/os_tempest/tasks/tempest_resources.yml') as f:
-  flavor = f.read()
-
+  
+config = configparser.ConfigParser()
+config.read('~/tempest/etc/tempest.conf')
+  
 args_yaml = yaml.safe_load(args)
 data_yaml = yaml.safe_load(data)
 cinder_yaml = yaml.safe_load(cinder_backends)
-flavor_yaml = yaml.safe_load(flavor)
+flavor = config.get('orchestration', 'instance_type')
 
 args_yaml['image_name'] = data_yaml['tempest_img_name']
-args_yaml['flavor_name'] = flavor_yaml[0][0]['register']
+args_yaml['flavor_name'] = flavor
 
 temp = bool(cinder_yaml['cinder_backend_lvm_inuse'])
 if temp == True:      
