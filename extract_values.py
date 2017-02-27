976cfebed78598611825110533fff5fe1d619a60
jwatford <jonte.watford@intel.com>
2017-02-27 15:34:31 -0600

    Update extract_values.py

--- extract_values.py
+++ extract_values.py
@@ -6,7 +6,7 @@ with open('args.yaml') as f:
   args = f.read()
   
 config = ConfigParser.ConfigParser()
-config.read('/root/tempest.conf')
+config.read('/root/etc/tempest.conf')
 config.sections()
   
 args_yaml = yaml.safe_load(args)
